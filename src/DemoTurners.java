4f44884c3405911f353ec4b2997cfd417cb45a04
Alireza Badrloo <arbadrloo@gmail.com>
2023-03-15 11:35:31 +0330

    updated class "DemoTurners" for new class "Window".
    made a new object out of window and added it to the "turners" array. so it can be iterated in for loop too.

--- src/DemoTurners.java
+++ src/DemoTurners.java
@@ -1,10 +1,6 @@
 public class DemoTurners {
     public static void main(String[] args) {
-        Turner[] turners = new Turner[] {
-                new Leaf(),
-                new Page(),
-                new Pancake(),
-        };
+        Turner[] turners = new Turner[] {new Leaf(), new Page(), new Pancake(), new Window()};
 
         for (Turner turner : turners) {
             turner.turn();
