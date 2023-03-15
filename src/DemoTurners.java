154f7d5f2e4bffe574bfce3033dc10230f9d60cc
Alireza Badrloo <arbadrloo@gmail.com>
2023-03-15 11:47:02 +0330

    updated "DemoTurners" to add an object from class "Wheel".
    I made class "Wheeel" and added and object out of it to the main class. as was asked by assignment.

--- src/DemoTurners.java
+++ src/DemoTurners.java
@@ -1,6 +1,6 @@
 public class DemoTurners {
     public static void main(String[] args) {
-        Turner[] turners = new Turner[] {new Leaf(), new Page(), new Pancake(), new Window()};
+        Turner[] turners = new Turner[] {new Leaf(), new Page(), new Pancake(), new Window(), new Wheel()};
 
         for (Turner turner : turners) {
             turner.turn();
