5dbbb606cae4bb07f3f5d6447f18d80cb9cb3e99
Alireza Badrloo <arbadrloo@gmail.com>
2023-03-15 12:14:20 +0330

    updated "DemoTurners2" for "Rozgar".
    added an object out of "Rozgar" sothat it can note us that we should not take the life very seriously.

--- src/DemoTurners2.java
+++ src/DemoTurners2.java
@@ -1,7 +1,7 @@
 public class DemoTurners2 {
     public static void main(String[] args) {
         Turner[] turners = new Turner[] {new Leaf(),new Page(),new Pancake(),
-                new Window(), new Wheel(), new Moustache()};
+                new Window(), new Wheel(), new Moustache(), new Rozgar()};
 
         for (Turner turner : turners) {
             turner.turn();
